5524b3afdb5d9f429087a724a170a37aa44049a1
Matthew Jin <mjin.code@gmail.com>
2023-07-24 12:06:43 -0700

    Return tweets by time and handle timeouts

--- twitscraper/scraper.py
+++ twitscraper/scraper.py
@@ -1,12 +1,15 @@
 import pickle
 import time
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
-from .utils import FIFOCache, get_links_js
+from .utils import get_links_js
 
 class Tweeter:
     def __init__(self, user, cookie_path):
@@ -14,32 +17,30 @@ class Tweeter:
         with open(cookie_path, "rb") as f:
             self.cookies = pickle.load(f)
         self._get_links_js = get_links_js(user)
-        self.latest = "1970-01-01T00:00:00Z"
-        self._init_latest()
-
-    def _init_latest(self):
-        """Checks twitter feed and updates latest time"""
-        driver = self._init_driver()
-        links = driver.execute_script(self._get_links_js)
-        self.latest = max(links.values())
-        driver.quit()
+        self.latest = datetime.utcnow().isoformat(sep='T', timespec='milliseconds') + "Z"
 
     def _init_driver(self):
         """Start headless Firefox instance"""
         options = Options()
-        options.add_argument('-headless')
-        options.add_argument('-disable-blink-features=AutomationControlled')
+        options.add_argument('--headless')
+        options.add_argument('--disable-blink-features=AutomationControlled')
         driver = webdriver.Firefox(options=options)
         driver.get("https://twitter.com")
         for c in self.cookies:
             driver.add_cookie(c)
         driver.get(f"https://twitter.com/{self.user}")
-        wait = WebDriverWait(driver, 30, 1).until(lambda x: x.find_element(By.CSS_SELECTOR, '[data-testid="tweet"]').is_displayed())
-        return driver
+        try:
+            wait = WebDriverWait(driver, 60, 1).until(lambda x: x.find_element(By.CSS_SELECTOR, '[data-testid="tweet"]').is_displayed())
+            return driver
+        except TimeoutException:
+            driver.quit()
+            return None
 
     def peek(self):
         """Peek at feed for new tweets"""
         driver = self._init_driver()
+        if driver is None:
+            return {}
         tweets = {}
         while all(ti > self.latest for tw, ti in tweets.items()):
             links = driver.execute_script(self._get_links_js)
@@ -49,4 +50,4 @@ class Tweeter:
         tweets = {tw: ti for tw, ti in tweets.items() if ti > self.latest}
         if len(tweets) > 0:
             self.latest = max(tweets.values())
-        return reversed(tweets.items())
+        return sorted(list(tweets.items()), key=lambda x: x[1])
