5c0e47f754ecbde07a6a6ded964bc5807de761c3
Matthew Jin <mjin.code@gmail.com>
2023-07-25 22:34:44 -0700

    Update scraper to handle multiple users

--- twitscraper/scraper.py
+++ twitscraper/scraper.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+import logging
 import pickle
 import time
-from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,42 +13,115 @@ from selenium.common.exceptions import TimeoutException
 from .utils import get_links_js
 
 class Tweeter:
-    def __init__(self, user, cookie_path):
-        self.user = user
+    logger = logging.getLogger('Tweeter')
+
+    def __init__(self, cookie_path):
         with open(cookie_path, "rb") as f:
             self.cookies = pickle.load(f)
-        self._get_links_js = get_links_js(user)
-        self.latest = datetime.utcnow().isoformat(sep='T', timespec='milliseconds') + "Z"
+        # self._get_links_js = get_links_js(user)
+        self.users = {}
+        self.driver = None
 
-    def _init_driver(self):
-        """Start headless Firefox instance"""
+    def _start(self, user='elonmusk'):
+        # NOTE: using the muskrat's homepage as test page
+        """
+        Start headless Firefox instance.
+
+        Stops and raises TimeoutException if twitter fails to load.
+        """
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-blink-features=AutomationControlled')
-        driver = webdriver.Firefox(options=options)
-        driver.get("https://twitter.com")
-        for c in self.cookies:
-            driver.add_cookie(c)
-        driver.get(f"https://twitter.com/{self.user}")
+        self.driver = webdriver.Firefox(options=options)
         try:
-            wait = WebDriverWait(driver, 60, 1).until(lambda x: x.find_element(By.CSS_SELECTOR, '[data-testid="tweet"]').is_displayed())
-            return driver
-        except TimeoutException:
-            driver.quit()
-            return None
+            self.driver.get("https://twitter.com")
+            for c in self.cookies:
+                self.driver.add_cookie(c)
+            self._get(user)
+        except TimeoutException as e:
+            logger.error("Failed to log in to twitter. Stopping webdriver.")
+            self._stop()
+            raise
 
-    def peek(self):
-        """Peek at feed for new tweets"""
-        driver = self._init_driver()
-        if driver is None:
-            return {}
+    def _get(self, user, wait_timeout=60, poll_frequency=1):
+        """
+        Gets a twitter user's feed and waits for it to load. 
+        
+        Throws TimeoutException if feed fails to load within the timeout.
+        """
+        self.driver.get(f"https://twitter.com/{user}")
+        try:
+            wait = WebDriverWait(self.driver, wait_timeout, poll_frequency).\
+                    until(lambda x: x.find_element(By.CSS_SELECTOR, '[data-testid="tweet"]').is_displayed())
+        except TimeoutException as e:
+            logger.error("Failed to access/find tweets at https://twitter.com/{user}")
+            raise
+
+    def _stop(self):
+        """
+        Stops the webdriver.
+        """
+        self.driver.quit()
+        self.driver = None
+
+    def add_user(self, user, check=False):
+        """
+        Adds a user to the Tweeter userlist.
+
+        check queries the user's account to see if their tweets are visible
+        but also induces significant I/O overhead
+
+        Returns the time when the user was added to the tweetlist; None if
+        user's tweets cannot be accessed.
+        """
+        if check:
+            try:
+                self._start(user)
+            except TimeoutException as e:
+                return None
+            self._stop()
+        self.users[user] = datetime.utcnow().isoformat(sep='T', timespec='milliseconds')
+        return self.users[user]
+
+    def remove_user(self, user):
+        """
+        Removes a user from the Tweeter userlist.
+
+        Returns None if the user does not exist.
+        """
+        return self.users.pop(user, None)
+
+    def peek(self, user=None, limit=10):
+        """
+        Checks the user(s) feed for new tweets.
+
+        limit dictates the maximum number of script executions
+        """
         tweets = {}
-        while all(ti > self.latest for tw, ti in tweets.items()):
-            links = driver.execute_script(self._get_links_js)
+        self._start()
+        if user is None:
+            for u in self.users.keys():
+                tweets[u] = self._peek(u, limit)
+        else:
+            tweets[user] = self._peek(user, limit)
+        self._stop()
+        for k, v in tweets.items():
+            tweets[k] = sorted(list(v.items()), key=lambda x: x[1])
+        return tweets
+
+    def _peek(self, user, limit=10):
+        """
+        Helper method for peek.
+        """
+        self._get(user)
+        i, tweets = 0, {}
+        script = get_links_js(user)
+        while i < limit and sum(ti < self.users[user] for ti in tweets.values()) < 2:
+            links = self.driver.execute_script(script)
             tweets.update(links)
             time.sleep(1)
-        driver.quit()
-        tweets = {tw: ti for tw, ti in tweets.items() if ti > self.latest}
+            i += 1
+        tweets = {tw: ti for tw, ti in tweets.items() if ti > self.users[user]}
         if len(tweets) > 0:
-            self.latest = max(tweets.values())
-        return sorted(list(tweets.items()), key=lambda x: x[1])
+            self.users[user] = max(tweets.values())
+        return tweets
--- twitscraper/utils.py
+++ twitscraper/utils.py
@@ -9,13 +9,13 @@ def get_links_js(user):
         var user_tweets = []
         for (let i = 0; i < tweets.length; i++) {{
             let t = tweets[i]
-            if (t.querySelector('a[href^="/{user}/status/"]') && t.childNodes[0].childNodes[0].childNodes[0].childNodes[0].childElementCount == 1 && t.querySelector('[data-testid="socialContext"]') == null) {{
+            if (t.querySelector('a[href^="/{user}/status/" i]') && t.childNodes[0].childNodes[0].childNodes[0].childNodes[0].childElementCount == 1) {{
                 user_tweets.push(t)
             }}
         }}
         var links = {{}}
         for (let i = 0; i < user_tweets.length; i++) {{
-            let t = user_tweets[i].querySelector('a[href^="/{user}/status/"]')
+            let t = user_tweets[i].querySelector('a[href^="/{user}/status/" i]')
             links[t.getAttribute('href')] = t.childNodes[0].getAttribute('datetime')
         }}
         if (last != null) {{
